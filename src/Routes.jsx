d5f6af17f01dbdcf9872fd588e2707db56af44e3
white3d <odhiambo.edward111@gmail.com>
2023-04-07 10:47:44 +0300

    Delete Pages

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,17 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
-const Shadow = React.lazy(() => import("pages/Shadow"));
-const Colors = React.lazy(() => import("pages/Colors"));
-const Color = React.lazy(() => import("pages/Color"));
-const Grid = React.lazy(() => import("pages/Grid"));
-const WebMockupTen = React.lazy(() => import("pages/WebMockupTen"));
-const Homepagemockup = React.lazy(() => import("pages/Homepagemockup"));
-const WebMockupThree = React.lazy(() => import("pages/WebMockupThree"));
-const ECommerceHomePagefordribbble = React.lazy(() =>
-  import("pages/ECommerceHomePagefordribbble")
-);
-const WebMockupEight = React.lazy(() => import("pages/WebMockupEight"));
 const ProductDetailspage = React.lazy(() => import("pages/ProductDetailspage"));
 const Error = React.lazy(() => import("pages/Error"));
 const Contact = React.lazy(() => import("pages/Contact"));
@@ -21,7 +10,6 @@ const Account = React.lazy(() => import("pages/Account"));
 const CheckOut = React.lazy(() => import("pages/CheckOut"));
 const Cart = React.lazy(() => import("pages/Cart"));
 const Wishlist = React.lazy(() => import("pages/Wishlist"));
-const CategoryDropdown = React.lazy(() => import("pages/CategoryDropdown"));
 const AccountDropdownWithaccount = React.lazy(() =>
   import("pages/AccountDropdownWithaccount")
 );
@@ -41,7 +29,6 @@ const ProjectRoutes = () => {
             path="/accountdropdownwithaccount"
             element={<AccountDropdownWithaccount />}
           />
-          <Route path="/categorydropdown" element={<CategoryDropdown />} />
           <Route path="/wishlist" element={<Wishlist />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<CheckOut />} />
@@ -50,18 +37,6 @@ const ProjectRoutes = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="/error" element={<Error />} />
           <Route path="/productdetailspage" element={<ProductDetailspage />} />
-          <Route path="/webmockupeight" element={<WebMockupEight />} />
-          <Route
-            path="/ecommercehomepagefordribbble"
-            element={<ECommerceHomePagefordribbble />}
-          />
-          <Route path="/webmockupthree" element={<WebMockupThree />} />
-          <Route path="/homepagemockup" element={<Homepagemockup />} />
-          <Route path="/webmockupten" element={<WebMockupTen />} />
-          <Route path="/grid" element={<Grid />} />
-          <Route path="/color" element={<Color />} />
-          <Route path="/colors" element={<Colors />} />
-          <Route path="/shadow" element={<Shadow />} />
           <Route path="/dhiwise-dashboard" element={<Home />} />
         </Routes>
       </Router>
